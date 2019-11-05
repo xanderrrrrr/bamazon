1205e3175642c532fca865f95947f298b2a8b5e7
Xander C <xander.canedo@gmail.com>
2019-11-05 11:18:21 -0700

    functioning database and select query

---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+// The app should then prompt users with two messages.
+
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(results);
+  });
+});
+
+  
